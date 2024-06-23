c6bd1e50df0995336ed9ef0dbe2811cd4463d783
boudjenah36 <31546201+boudjenah36@users.noreply.github.com>
2024-06-23 10:33:36 +0100

    corret print for generate file and make inferance patch size a parameter in yaml file

--- generation/generation.py
+++ generation/generation.py
@@ -11,10 +11,9 @@ from datasets import load_dataset,Dataset ,DatasetDict,load_from_disk
 import time
 import re
 from peft import PeftModel
-
-
-
 import yaml
+
+print("Imported libs \n")
 
 # Define a function to load parameters from a YAML file
 def load_config_from_yaml(file_path):
@@ -39,8 +38,10 @@ num_return_sequences_value = config["num_return_sequences_value"]
 max_new_tokens_value = config["max_new_tokens_value"]
 temperature_value = config["temperature_value"]
 top_p_value = config["top_p_value"]
+inferance_batch_size = config["inferance_batch_size"]
 hub_token = config["hub_token"]
 
+print("Loaded parameters \n")
 
 login(token=hub_token)
 
@@ -52,16 +53,23 @@ tokenizer.pad_token_id = 0
 tokenizer.padding_side ="left"
 
 
-dataset_p=load_dataset(dataset_path)
+
 
 print("tokenizer configered")
 
 
+dataset_p=load_dataset(dataset_path)
 
-# Apply the function to each row of the DataFrame
 val_data =dataset_p['test']
 
 
+
+val_data_prompts = list(val_data['text'])
+for i in range(len(val_data_prompts)):
+    parts= val_data_prompts[i].split("\n### Response:\n")
+    val_data_prompts[i]=parts[0]+"\n### Response:\n"
+
+print("Dataset loaded \n")
 
 compute_dtype = getattr(torch, "float16")
 quant_config = BitsAndBytesConfig(
@@ -71,7 +79,7 @@ quant_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=False,
 )
 
-
+print("Quantization configured \n")
 
 # Load the model and the tokenizer. Set generation config
 
@@ -94,6 +102,8 @@ else:
     device_map="auto"
     )
 
+print("Model loaded and configured \n")
+
 
 generation_config = GenerationConfig(
     do_sample=do_sample_value,
@@ -106,6 +116,7 @@ generation_config = GenerationConfig(
 )
 
 
+print("Generation config set \n")
 
 def solve_question(question_prompt):
     inputs = tokenizer(question_prompt, return_tensors="pt", padding=True, truncation=True,max_length= 2048).to("cuda")
@@ -114,20 +125,16 @@ def solve_question(question_prompt):
     return answer
 
 
+
+
+print("Inference starts\n")
+
+
+
 all_answers = []
-val_data_prompts = list(val_data['text'])
-for i in range(len(val_data_prompts)):
-    parts= val_data_prompts[i].split("\n### Response:\n")
-    val_data_prompts[i]=parts[0]+"\n### Response:\n"
-
-print("inference starts\n")
-
-
-
-
-for i in range(0, len(val_data_prompts), 4):
+for i in range(0, len(val_data_prompts), inferance_batch_size):
     print("iteration: ", i)
-    question_prompts = val_data_prompts[i:i+4]
+    question_prompts = val_data_prompts[i:i+inferance_batch_size]
     
     # Record start time
     start_time = time.time()
@@ -140,7 +147,7 @@ for i in range(0, len(val_data_prompts), 4):
     execution_time = end_time - start_time
     
     # Print execution time for the iteration
-    print("Execution time: {:.4f} seconds".format(execution_time))
+    print("iteration time: {:.4f} seconds".format(execution_time))
     
     # Clear CUDA memory cache
     torch.cuda.empty_cache()
@@ -152,3 +159,5 @@ val_data=val_data.add_column("finetuned_answers",all_answers)
 
 val_data.save_to_disk(updated_dataset_path)
 
+
+print("Data saved \n")
